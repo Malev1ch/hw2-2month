0c877b052fbf2513b2114711643ee13fd066e6dd
Malev1ch <shadowqq@rambler.ru>
2023-12-29 16:23:04 +0600

    add output text console log

--- main.js
+++ main.js
@@ -6,16 +6,20 @@ let btnReset = document.querySelector('.btn_reset');
 
 btnRed.addEventListener('click', () => {
   bodyColor.style.backgroundColor = 'red';
+  console.log("Body color changed to Red");
 });
 
 btnYellow.addEventListener('click', () => {
   bodyColor.style.backgroundColor = 'yellow';
+  console.log("Body color changed to Yellow");
 });
 
 btnGreen.addEventListener('click', () => {
   bodyColor.style.backgroundColor = 'green';
+  console.log("Body color changed to Green");
 });
 
 btnReset.addEventListener('click', () => {
   bodyColor.style.backgroundColor = '';
+  console.log("Body color dropped to default");
 });
